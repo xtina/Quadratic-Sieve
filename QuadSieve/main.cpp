3fe4116ad974ad0d3e1cf1b029286515bbf9f319
Christina <cib5224@psu.edu>
2014-12-02 19:34:07 -0500

    Initial Commit

---
+++ QuadSieve/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuadSieve
+//
+//  Created by Christina Black on 12/2/14.
+//  Copyright (c) 2014 Christina Black. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
